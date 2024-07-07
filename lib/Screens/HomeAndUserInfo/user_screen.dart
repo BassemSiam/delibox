1d38f93498b595e335b393ec11e3398e46b3a364
BassemSiam <bassemsiam644@gmail.com>
2024-07-07 19:34:16 +0300

    c

--- lib/Screens/HomeAndUserInfo/user_screen.dart
+++ lib/Screens/HomeAndUserInfo/user_screen.dart
@@ -1,7 +1,6 @@
 import 'package:delibox/components/const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../components/cash_helper.dart';
 import '../../components/components.dart';
 import '../../generated/l10n.dart';
