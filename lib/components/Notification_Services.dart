a992b1ce7981205f2aed9ec694a64dd9af001456
BassemSiam <bassemsiam644@gmail.com>
2024-10-05 20:01:08 +0300

    re

--- lib/components/Notification_Services.dart
+++ lib/components/Notification_Services.dart
@@ -1,84 +1,84 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:googleapis_auth/auth_io.dart' as auth;
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:googleapis_auth/auth_io.dart' as auth;
 
-class PushNotificationService {
-  static Future<String?> getAccessToken() async {
-    final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "delibox-36e5f",
-      "private_key_id": "ad0aa5bcf3126a441f14e99cf8f7e5d72ef80d7d",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQClN9qFxU94/yeX\nGD0LsSGyTn+g5fwI098lxT5Ge7wTSO2tU8MYBt5XKy95dgAZlOZy1WHs9fjwLeyH\nr/bI6FVA7ezho6A456j6oJXoMJ+fvXwICxowX+NMGu6qocEM79N/D1REJr3ZpcqN\nJ/Cxfjly77qPWh5QzuX9EFJIasRhpO7xtmgXGySwOTPvUfvy9oDqhExPxx5lpqmG\nWDoV2peQLh+tNdTWDkz8/c2Ti4yBTi9Cnf8CMIRb7DWKF2mGH/ltGQri/lbiWL7Y\nz79ZmDBEAXL19YXfSbR9DE2DG7D6IxvRto1t4i4LGxUmtWutg09nBMrr8WzWJeXR\nrZWNxP2VAgMBAAECgf8Aq5Hybx9jaBu3KJM+M/m9yUT2WEdBJF/NM7q0U2m00bRz\n4gCjrAD/z60JAlP9xpMBWmYVI683cZXI9x7/Ty0JW1AuXw89ep9mFhlKfzbEFoCv\nOOYnOGO9fvwCtS8dIz5fxZKbx9s4X2ny7Y8p7hQ54yGF/TA1/FIZ5e00cbCVUlk1\noWB6WmNT/4P+Va+VgMJSWavyziSURoW2PHshv1MT4TvhnKq5SZeWbDm6bBW+ucu0\n5GP8tP58Hp1iBZQT2nWmVacol99YKMcWI1cgB51x3IOZIP+N7ipSPMUbkHw60Fnh\nCOFt9mx3sVFimzkfmOJZQkghISNSPWTZe5sZRvECgYEA0gER/u9gp1sYwyGXFNX6\nMw88Vzi1x7iyRZjH1CGuQZZ8hyxwr+YTMx2dc4JdpoBC8OgH+DaqyF1BTU7vBLo+\npPPZsAFIoe7lK24VFFHpUFyQ+1OsoZk7cixxcl9JI2vAGdEckxBUlLgDdGI9Qbcf\nlewRvIbwTJcPY8gDiXCq0L0CgYEAyWeiFZA6WiVZ7iIJ8awQB06L90wqOkXR4k8C\nCzckIkwPdJPMOnZ90GBx+/Y/g+PbRYsVxbzyqSUEIIHJoaPOsoYRLSnBBrK9yJvy\nowtERQA4PzysYb/0iQnGvvdTWAJFmY6JYf94429MhBmDYxkycX2aA/+ixBrX+jsA\n5QqLibkCgYA32h0FJnkYSXweeZKK8hXm+ohb0jNWeOLA689JteN4giOAd+tIfPJi\nR+AWVne9zqL/G1bzRubI/zRc8T9UjIwYptogm1bL6pN8p3xnuRlKTDQrA6gIGo7a\n4MMTXhA/oWqn8xucaV11aDNTAsWz8pYt82kMMP87/3kSWqHGLfbgKQKBgQDDRIrN\nRUyN4fDTBIWHnj7cFrd1SI8YeGEJfXiJzyhlaEED0dgMRp2szhU1KIZkJSKOxk6R\nrKPIgm8B5VPMN7lLNNiqZPUUvW9rdGdG1MGX9KKUDQtEqos34hajR7+ycGFyg9OH\nA939BAe3e9T6248goZoVjmAMqcoEffhcGGE5YQKBgQCBDu9tHxeGr7pknF22DWup\njaLBDp41pTQwQlBeyxdH8XWIILQMDXqDJBXmdlzLWxnuqfqvXM5KqkNvjDHpbR3I\nT9J5md6trRoMsDisVIaXUCGX6wOx6hp93VOwhGs1rocFo97crxV5uS5eXM6ieSD2\nXjlhue2eeuR/FK5W4pG68A==\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "delibox-bassem-siam@delibox-36e5f.iam.gserviceaccount.com",
-      "client_id": "116975212250233451356",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/delibox-bassem-siam%40delibox-36e5f.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
+// class PushNotificationService {
+//   static Future<String?> getAccessToken() async {
+//     final serviceAccountJson = {
+//       "type": "service_account",
+//       "project_id": "delibox-36e5f",
+//       "private_key_id": "ad0aa5bcf3126a441f14e99cf8f7e5d72ef80d7d",
+//       "private_key":
+//           "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQClN9qFxU94/yeX\nGD0LsSGyTn+g5fwI098lxT5Ge7wTSO2tU8MYBt5XKy95dgAZlOZy1WHs9fjwLeyH\nr/bI6FVA7ezho6A456j6oJXoMJ+fvXwICxowX+NMGu6qocEM79N/D1REJr3ZpcqN\nJ/Cxfjly77qPWh5QzuX9EFJIasRhpO7xtmgXGySwOTPvUfvy9oDqhExPxx5lpqmG\nWDoV2peQLh+tNdTWDkz8/c2Ti4yBTi9Cnf8CMIRb7DWKF2mGH/ltGQri/lbiWL7Y\nz79ZmDBEAXL19YXfSbR9DE2DG7D6IxvRto1t4i4LGxUmtWutg09nBMrr8WzWJeXR\nrZWNxP2VAgMBAAECgf8Aq5Hybx9jaBu3KJM+M/m9yUT2WEdBJF/NM7q0U2m00bRz\n4gCjrAD/z60JAlP9xpMBWmYVI683cZXI9x7/Ty0JW1AuXw89ep9mFhlKfzbEFoCv\nOOYnOGO9fvwCtS8dIz5fxZKbx9s4X2ny7Y8p7hQ54yGF/TA1/FIZ5e00cbCVUlk1\noWB6WmNT/4P+Va+VgMJSWavyziSURoW2PHshv1MT4TvhnKq5SZeWbDm6bBW+ucu0\n5GP8tP58Hp1iBZQT2nWmVacol99YKMcWI1cgB51x3IOZIP+N7ipSPMUbkHw60Fnh\nCOFt9mx3sVFimzkfmOJZQkghISNSPWTZe5sZRvECgYEA0gER/u9gp1sYwyGXFNX6\nMw88Vzi1x7iyRZjH1CGuQZZ8hyxwr+YTMx2dc4JdpoBC8OgH+DaqyF1BTU7vBLo+\npPPZsAFIoe7lK24VFFHpUFyQ+1OsoZk7cixxcl9JI2vAGdEckxBUlLgDdGI9Qbcf\nlewRvIbwTJcPY8gDiXCq0L0CgYEAyWeiFZA6WiVZ7iIJ8awQB06L90wqOkXR4k8C\nCzckIkwPdJPMOnZ90GBx+/Y/g+PbRYsVxbzyqSUEIIHJoaPOsoYRLSnBBrK9yJvy\nowtERQA4PzysYb/0iQnGvvdTWAJFmY6JYf94429MhBmDYxkycX2aA/+ixBrX+jsA\n5QqLibkCgYA32h0FJnkYSXweeZKK8hXm+ohb0jNWeOLA689JteN4giOAd+tIfPJi\nR+AWVne9zqL/G1bzRubI/zRc8T9UjIwYptogm1bL6pN8p3xnuRlKTDQrA6gIGo7a\n4MMTXhA/oWqn8xucaV11aDNTAsWz8pYt82kMMP87/3kSWqHGLfbgKQKBgQDDRIrN\nRUyN4fDTBIWHnj7cFrd1SI8YeGEJfXiJzyhlaEED0dgMRp2szhU1KIZkJSKOxk6R\nrKPIgm8B5VPMN7lLNNiqZPUUvW9rdGdG1MGX9KKUDQtEqos34hajR7+ycGFyg9OH\nA939BAe3e9T6248goZoVjmAMqcoEffhcGGE5YQKBgQCBDu9tHxeGr7pknF22DWup\njaLBDp41pTQwQlBeyxdH8XWIILQMDXqDJBXmdlzLWxnuqfqvXM5KqkNvjDHpbR3I\nT9J5md6trRoMsDisVIaXUCGX6wOx6hp93VOwhGs1rocFo97crxV5uS5eXM6ieSD2\nXjlhue2eeuR/FK5W4pG68A==\n-----END PRIVATE KEY-----\n",
+//       "client_email":
+//           "delibox-bassem-siam@delibox-36e5f.iam.gserviceaccount.com",
+//       "client_id": "116975212250233451356",
+//       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//       "token_uri": "https://oauth2.googleapis.com/token",
+//       "auth_provider_x509_cert_url":
+//           "https://www.googleapis.com/oauth2/v1/certs",
+//       "client_x509_cert_url":
+//           "https://www.googleapis.com/robot/v1/metadata/x509/delibox-bassem-siam%40delibox-36e5f.iam.gserviceaccount.com",
+//       "universe_domain": "googleapis.com"
+//     };
 
-    List<String> scopes = [
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/firebase.database",
-      "https://www.googleapis.com/auth/firebase.messaging"
-    ];
-     try {
-      http.Client client = await auth.clientViaServiceAccount(
-          auth.ServiceAccountCredentials.fromJson(serviceAccountJson), scopes);
+//     List<String> scopes = [
+//       "https://www.googleapis.com/auth/userinfo.email",
+//       "https://www.googleapis.com/auth/firebase.database",
+//       "https://www.googleapis.com/auth/firebase.messaging"
+//     ];
+//      try {
+//       http.Client client = await auth.clientViaServiceAccount(
+//           auth.ServiceAccountCredentials.fromJson(serviceAccountJson), scopes);
 
-      auth.AccessCredentials credentials =
-          await auth.obtainAccessCredentialsViaServiceAccount(
-              auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
-              scopes,
-              client);
+//       auth.AccessCredentials credentials =
+//           await auth.obtainAccessCredentialsViaServiceAccount(
+//               auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+//               scopes,
+//               client);
 
-      client.close();
-      print(
-          "Access Token: ${credentials.accessToken.data}"); // Print Access Token
-      return credentials.accessToken.data;
-    } catch (e) {
-      print("Error getting access token: $e");
-      return null;
-    }
-  }
+//       client.close();
+//       print(
+//           "Access Token: ${credentials.accessToken.data}"); // Print Access Token
+//       return credentials.accessToken.data;
+//     } catch (e) {
+//       print("Error getting access token: $e");
+//       return null;
+//     }
+//   }
 
-  static sendNotificationToSelectDriver(
-      String deviceToken, BuildContext context, String tripId) async {
-    final String? serverkey = await getAccessToken();
+//   static sendNotificationToSelectDriver(
+//       String deviceToken, BuildContext context, String tripId) async {
+//     final String? serverkey = await getAccessToken();
 
-    const String FCMEndPoint =
-        "https://fcm.googleapis.com/v1/projects/delibox-36e5f/messages:send";
+//     const String FCMEndPoint =
+//         "https://fcm.googleapis.com/v1/projects/delibox-36e5f/messages:send";
 
-    final Map<String, dynamic> message = {
-      'message': {
-        'token': deviceToken,
-      },
-      "notification": {
-        "title": 'dwwdw',
-        "body": 'dwwdwdw11111',
-      },
-      "data": {'tripID': tripId}
-    };
+//     final Map<String, dynamic> message = {
+//       'message': {
+//         'token': deviceToken,
+//       },
+//       "notification": {
+//         "title": 'dwwdw',
+//         "body": 'dwwdwdw11111',
+//       },
+//       "data": {'tripID': tripId}
+//     };
 
-    final http.Response response = await http.post(
-      Uri.parse(FCMEndPoint),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $serverkey'
-      },
-      body: jsonEncode(message),
-    );
+//     final http.Response response = await http.post(
+//       Uri.parse(FCMEndPoint),
+//       headers: <String, String>{
+//         'Content-Type': 'application/json',
+//         'Authorization': 'Bearer $serverkey'
+//       },
+//       body: jsonEncode(message),
+//     );
 
-    if (response.statusCode == 200) {
-      print('Notification sent successfully');
-    } else {
-      print('Notification filed , not sent : ${response.statusCode}');
-    }
-  }
-}
+//     if (response.statusCode == 200) {
+//       print('Notification sent successfully');
+//     } else {
+//       print('Notification filed , not sent : ${response.statusCode}');
+//     }
+//   }
+// }
--- lib/components/Notification_helper.dart
+++ lib/components/Notification_helper.dart
@@ -1,152 +1,152 @@
-import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
-import 'package:googleapis_auth/auth_io.dart' as auth;
-import 'package:firebase_messaging/firebase_messaging.dart';
-import '../components/components.dart';
+// import 'package:dio/dio.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:googleapis_auth/auth_io.dart' as auth;
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import '../components/components.dart';
 
-class NotificationsHelper {
-  // creat instance of fbm
-  final _firebaseMessaging = FirebaseMessaging.instance;
+// class NotificationsHelper {
+//   // creat instance of fbm
+//   final _firebaseMessaging = FirebaseMessaging.instance;
 
-  // initialize notifications for this app or device
-  Future<void> initNotifications() async {
-    await _firebaseMessaging.requestPermission();
-    // get device token
-    String? deviceToken = await _firebaseMessaging.getToken();
-    print(
-        "===================Device FirebaseMessaging Token====================");
-    print(deviceToken);
-    print(
-        "===================Device FirebaseMessaging Token====================");
-  }
+//   // initialize notifications for this app or device
+//   Future<void> initNotifications() async {
+//     await _firebaseMessaging.requestPermission();
+//     // get device token
+//     String? deviceToken = await _firebaseMessaging.getToken();
+//     print(
+//         "===================Device FirebaseMessaging Token====================");
+//     print(deviceToken);
+//     print(
+//         "===================Device FirebaseMessaging Token====================");
+//   }
 
-  // handle notifications when received
-  void handleMessages(RemoteMessage? message) {
-    if (message != null) {
-      // navigatorKey.currentState?.pushNamed(NotificationsScreen.routeName, arguments: message);
-      showToast(
-          text: 'on Background Message notification',
-          state: ToastStates.success);
-    }
-  }
+//   // handle notifications when received
+//   void handleMessages(RemoteMessage? message) {
+//     if (message != null) {
+//       // navigatorKey.currentState?.pushNamed(NotificationsScreen.routeName, arguments: message);
+//       showToast(
+//           text: 'on Background Message notification',
+//           state: ToastStates.success);
+//     }
+//   }
 
-  // handel notifications in case app is terminated
-  void handleBackgroundNotifications() async {
-    FirebaseMessaging.instance.getInitialMessage().then((handleMessages));
-    FirebaseMessaging.onMessageOpenedApp.listen(handleMessages);
-  }
+//   // handel notifications in case app is terminated
+//   void handleBackgroundNotifications() async {
+//     FirebaseMessaging.instance.getInitialMessage().then((handleMessages));
+//     FirebaseMessaging.onMessageOpenedApp.listen(handleMessages);
+//   }
 
-  Future<String?> getAccessToken() async {
-    final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "delibox-36e5f",
-      "private_key_id": "d0bfa4be95a81721371d4f1848072d39ddbbfe4c",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCnl8sWb8WVmCJ/\n32DLmAAQEmkdEzAUjHotUWCI0twyWkz7WRCbuBBCsL5oD8h05mi+Br3ES2rwYPFM\nPY2ptQS5xcDwBhKuZ36PZ4ZBAUAtbv+hTQaJLlbBWfFpoZIaMcwWZVMlaORKCp9R\nkWRRQrCJWmx56y51sSxgtB1CcnbvsJZSpFO5WvuwkNPEc/dPjtB38KiCVPkMTEdS\nHBN8U+3kLuVo7lFrP6QQ4ZnEC9G+YbePB8EOogTKRSRr+EbE8fdYNADtaxGgZU06\nEkB1POx9GbbRQuw0CEMFDZzN5q9NYl9f+kvxywjZ8tb7oZ0pbjqTQlCcs1MZ2Quk\n6NLDhqzDAgMBAAECggEALgugerue16kC4Ysn9wf+DGxigOCCt0KZVtYi5bS5VwRE\nXlJsI7yAizIwPjsH4kZjxMHpZYMrEHwyD7Zhs5zOfZP9nHSusUgyF4hj9Hw5e8Ih\nbdnRz+LxZxIlJ9aCY7wGzKhuHbsh7c3ZU/nUTBc8laBIaX7aNu/Et5UE7Pqx/sxc\n9/+36W+jQlS8+gvj7F8VO/LPBNhNBYkCtprvOIVRuac9na2Qo4A+JziP3I+9wYX2\nbKSsoUmzid28AgAzO6IVtsEE/NXFowqug6VKyf4I5Or7p6c/mlF75yE/3Y85OX6H\nGdHzXmi3s2uuFzRxku4mL1bzqxXyxai7lDABJ8gewQKBgQDp311LB2iLxAPk2pr5\nibC1PDTW70ZMTZ7R/WQz774+QJeP6cLe2yOr777R3+JdlCer0x4/NjIgXLBlVa1b\nHRf2Ssyxqf+ZReRv2HIkaap9gSVlPvdtqSZ7VcMs1AbsssBNX2B5Oe7JxBOaLC8Z\numMJKES+9JNFjjCYMxIbeQ/kEwKBgQC3cxGT1uP0NDToj7JKr49utCe3MRSBf2GB\nR3Gyvk9OWWP9pejnum+3E+6PD+3SNbftnN+8ZMJI+vEMUfmFqWzZ/80cSBKE0y1F\ntF+6AvkSNz42hVDSfRqeTZmr581okZgjV701LvHa/g1k+fMy8XqV24Uap+c2o7/d\nH9aVSSNKkQKBgQCHv3bPTgWI2sXH2HuLzDgdekzRVSFguM0u5f17h/TM/YD7r5vE\n7z3NVuNTvrgNySkduvUbi8GctzBMIWb2M/TsqI4Xsa0Csmbd5KycJhknsAactee4\nZy5G+IjJRGigi0DDqFC/DLlls8INAwhzzpQdhJoinKO/A8GBFmO0PpjSsQKBgH+w\nn46JKBhF8oHh65cixjKnQXG4vu44bPUgdpqU7vy5KelW/1u0oKgNPqLj0oTcEU9a\nPz2R4M4NpZUOxiDsQjEnZWyHGiTMHRoEqBEAmr+8JhirCQvKOqnvl8RZT9e2Z/BJ\n1RBcwcnNxiL/+/D8tnfy2Z4pOr4Ekrrmu2UkCGWRAoGAamSw8CTQq/O9hHX6640G\ns6V70otRUQYpj0yf+2mNPZx8rkawjlINZtKHE315FLcay6aYqDVxJr49j4s32DTL\n3mVV+qiQJX23yHuU47EEhBQw8uC3JBDKnWydDXt0eXVBJCX1b0JFpnuAOzew4QBY\nbg38HdEprG7TpmMKj4J3FMs=\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "firebase-adminsdk-2nplq@delibox-36e5f.iam.gserviceaccount.com",
-      "client_id": "113790320477453784860",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2nplq%40delibox-36e5f.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
+//   Future<String?> getAccessToken() async {
+//     final serviceAccountJson = {
+//       "type": "service_account",
+//       "project_id": "delibox-36e5f",
+//       "private_key_id": "d0bfa4be95a81721371d4f1848072d39ddbbfe4c",
+//       "private_key":
+//           "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCnl8sWb8WVmCJ/\n32DLmAAQEmkdEzAUjHotUWCI0twyWkz7WRCbuBBCsL5oD8h05mi+Br3ES2rwYPFM\nPY2ptQS5xcDwBhKuZ36PZ4ZBAUAtbv+hTQaJLlbBWfFpoZIaMcwWZVMlaORKCp9R\nkWRRQrCJWmx56y51sSxgtB1CcnbvsJZSpFO5WvuwkNPEc/dPjtB38KiCVPkMTEdS\nHBN8U+3kLuVo7lFrP6QQ4ZnEC9G+YbePB8EOogTKRSRr+EbE8fdYNADtaxGgZU06\nEkB1POx9GbbRQuw0CEMFDZzN5q9NYl9f+kvxywjZ8tb7oZ0pbjqTQlCcs1MZ2Quk\n6NLDhqzDAgMBAAECggEALgugerue16kC4Ysn9wf+DGxigOCCt0KZVtYi5bS5VwRE\nXlJsI7yAizIwPjsH4kZjxMHpZYMrEHwyD7Zhs5zOfZP9nHSusUgyF4hj9Hw5e8Ih\nbdnRz+LxZxIlJ9aCY7wGzKhuHbsh7c3ZU/nUTBc8laBIaX7aNu/Et5UE7Pqx/sxc\n9/+36W+jQlS8+gvj7F8VO/LPBNhNBYkCtprvOIVRuac9na2Qo4A+JziP3I+9wYX2\nbKSsoUmzid28AgAzO6IVtsEE/NXFowqug6VKyf4I5Or7p6c/mlF75yE/3Y85OX6H\nGdHzXmi3s2uuFzRxku4mL1bzqxXyxai7lDABJ8gewQKBgQDp311LB2iLxAPk2pr5\nibC1PDTW70ZMTZ7R/WQz774+QJeP6cLe2yOr777R3+JdlCer0x4/NjIgXLBlVa1b\nHRf2Ssyxqf+ZReRv2HIkaap9gSVlPvdtqSZ7VcMs1AbsssBNX2B5Oe7JxBOaLC8Z\numMJKES+9JNFjjCYMxIbeQ/kEwKBgQC3cxGT1uP0NDToj7JKr49utCe3MRSBf2GB\nR3Gyvk9OWWP9pejnum+3E+6PD+3SNbftnN+8ZMJI+vEMUfmFqWzZ/80cSBKE0y1F\ntF+6AvkSNz42hVDSfRqeTZmr581okZgjV701LvHa/g1k+fMy8XqV24Uap+c2o7/d\nH9aVSSNKkQKBgQCHv3bPTgWI2sXH2HuLzDgdekzRVSFguM0u5f17h/TM/YD7r5vE\n7z3NVuNTvrgNySkduvUbi8GctzBMIWb2M/TsqI4Xsa0Csmbd5KycJhknsAactee4\nZy5G+IjJRGigi0DDqFC/DLlls8INAwhzzpQdhJoinKO/A8GBFmO0PpjSsQKBgH+w\nn46JKBhF8oHh65cixjKnQXG4vu44bPUgdpqU7vy5KelW/1u0oKgNPqLj0oTcEU9a\nPz2R4M4NpZUOxiDsQjEnZWyHGiTMHRoEqBEAmr+8JhirCQvKOqnvl8RZT9e2Z/BJ\n1RBcwcnNxiL/+/D8tnfy2Z4pOr4Ekrrmu2UkCGWRAoGAamSw8CTQq/O9hHX6640G\ns6V70otRUQYpj0yf+2mNPZx8rkawjlINZtKHE315FLcay6aYqDVxJr49j4s32DTL\n3mVV+qiQJX23yHuU47EEhBQw8uC3JBDKnWydDXt0eXVBJCX1b0JFpnuAOzew4QBY\nbg38HdEprG7TpmMKj4J3FMs=\n-----END PRIVATE KEY-----\n",
+//       "client_email":
+//           "firebase-adminsdk-2nplq@delibox-36e5f.iam.gserviceaccount.com",
+//       "client_id": "113790320477453784860",
+//       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//       "token_uri": "https://oauth2.googleapis.com/token",
+//       "auth_provider_x509_cert_url":
+//           "https://www.googleapis.com/oauth2/v1/certs",
+//       "client_x509_cert_url":
+//           "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2nplq%40delibox-36e5f.iam.gserviceaccount.com",
+//       "universe_domain": "googleapis.com"
+//     };
 
-    List<String> scopes = [
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/firebase.database",
-      "https://www.googleapis.com/auth/firebase.messaging"
-    ];
+//     List<String> scopes = [
+//       "https://www.googleapis.com/auth/userinfo.email",
+//       "https://www.googleapis.com/auth/firebase.database",
+//       "https://www.googleapis.com/auth/firebase.messaging"
+//     ];
 
-    try {
-      http.Client client = await auth.clientViaServiceAccount(
-          auth.ServiceAccountCredentials.fromJson(serviceAccountJson), scopes);
+//     try {
+//       http.Client client = await auth.clientViaServiceAccount(
+//           auth.ServiceAccountCredentials.fromJson(serviceAccountJson), scopes);
 
-      auth.AccessCredentials credentials =
-          await auth.obtainAccessCredentialsViaServiceAccount(
-              auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
-              scopes,
-              client);
+//       auth.AccessCredentials credentials =
+//           await auth.obtainAccessCredentialsViaServiceAccount(
+//               auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+//               scopes,
+//               client);
 
-      client.close();
+//       client.close();
 
-      print(
-          "Access Token: ${credentials.accessToken.data}"); // Print Access Token
-      return credentials.accessToken.data;
-    } catch (e) {
-      print("Error getting access token: $e");
-      return null;
-    }
-  }
+//       print(
+//           "Access Token: ${credentials.accessToken.data}"); // Print Access Token
+//       return credentials.accessToken.data;
+//     } catch (e) {
+//       print("Error getting access token: $e");
+//       return null;
+//     }
+//   }
 
-  Map<String, dynamic> getBody({
-    required String fcmToken,
-    required String title,
-    required String body,
-    required String userId,
-    String? type,
-  }) {
-    return {
-      "message": {
-        "token": fcmToken,
-        "notification": {"title": title, "body": body},
-        "android": {
-          "notification": {
-            "notification_priority": "PRIORITY_MAX",
-            "sound": "default"
-          }
-        },
-        "apns": {
-          "payload": {
-            "aps": {"content_available": true}
-          }
-        },
-        "data": {
-          "type": type,
-          "id": userId,
-          "click_action": "FLUTTER_NOTIFICATION_CLICK"
-        }
-      }
-    };
-  }
+//   Map<String, dynamic> getBody({
+//     required String fcmToken,
+//     required String title,
+//     required String body,
+//     required String userId,
+//     String? type,
+//   }) {
+//     return {
+//       "message": {
+//         "token": fcmToken,
+//         "notification": {"title": title, "body": body},
+//         "android": {
+//           "notification": {
+//             "notification_priority": "PRIORITY_MAX",
+//             "sound": "default"
+//           }
+//         },
+//         "apns": {
+//           "payload": {
+//             "aps": {"content_available": true}
+//           }
+//         },
+//         "data": {
+//           "type": type,
+//           "id": userId,
+//           "click_action": "FLUTTER_NOTIFICATION_CLICK"
+//         }
+//       }
+//     };
+//   }
 
-  Future<void> sendNotifications({
-    required String fcmToken,
-    required String title,
-    required String body,
-    required String userId,
-    String? type,
-  }) async {
-    try {
-      var serverKeyAuthorization = await getAccessToken();
+//   Future<void> sendNotifications({
+//     required String fcmToken,
+//     required String title,
+//     required String body,
+//     required String userId,
+//     String? type,
+//   }) async {
+//     try {
+//       var serverKeyAuthorization = await getAccessToken();
 
-      // change your project id
-      const String urlEndPoint =
-          "https://fcm.googleapis.com/v1/projects/delibox-36e5f/messages:send";
+//       // change your project id
+//       const String urlEndPoint =
+//           "https://fcm.googleapis.com/v1/projects/delibox-36e5f/messages:send";
 
-      Dio dio = Dio();
-      dio.options.headers['Content-Type'] = 'application/json';
-      dio.options.headers['Authorization'] = 'Bearer $serverKeyAuthorization';
+//       Dio dio = Dio();
+//       dio.options.headers['Content-Type'] = 'application/json';
+//       dio.options.headers['Authorization'] = 'Bearer $serverKeyAuthorization';
 
-      var response = await dio.post(
-        urlEndPoint,
-        data: getBody(
-          userId: userId,
-          fcmToken: fcmToken,
-          title: title,
-          body: body,
-          type: type ?? "message",
-        ),
-      );
+//       var response = await dio.post(
+//         urlEndPoint,
+//         data: getBody(
+//           userId: userId,
+//           fcmToken: fcmToken,
+//           title: title,
+//           body: body,
+//           type: type ?? "message",
+//         ),
+//       );
 
-      // Print response status code and body for debugging
-      print('Response Status Code: ${response.statusCode}');
-      print('Response Data: ${response.data}');
-    } catch (e) {
-      print("Error sending notification: $e");
-    }
-  }
-}
+//       // Print response status code and body for debugging
+//       print('Response Status Code: ${response.statusCode}');
+//       print('Response Data: ${response.data}');
+//     } catch (e) {
+//       print("Error sending notification: $e");
+//     }
+//   }
+// }
